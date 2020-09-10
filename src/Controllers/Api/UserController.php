3150b6cba05dfcf4af53ade3a04686184c3f5aab
张晔 <y@qilephp.com>
2020-09-10 15:31:43 +0800

    update spread

--- src/Controllers/Api/UserController.php
+++ src/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace Qihucms\UCenter\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Spread;
 use App\Models\User;
 use App\Repositories\SpreadRepository;
 use Illuminate\Http\Request;
@@ -37,23 +38,21 @@ class UserController extends Controller
         }
         // 会员上级
         if (!empty($punionid)) {
-            $punionid = User::where('wechat->unionid', $punionid)->value('id') ?: 0;
+            $punionid = User::where('open_id->wechat->unionid', $punionid)->value('id') ?: 0;
         } elseif (!empty($pmobile)) {
             $punionid = User::where('username', $pmobile)->orWhere('mobile', $pmobile)->value('id') ?: 0;
         } else {
             $punionid = 0;
         }
 
-        $isCreate = false;
         // 更新或创建会员数据
         if (!empty($unionid)) {
-            $user = User::where('wechat->unionid', $unionid)->first();
+            $user = User::where('open_id->wechat->unionid', $unionid)->first();
             if ($user) {
                 $user->username = empty($user->username) ? $mobile : $user->username;
                 $user->mobile = empty($user->mobile) ? $mobile : $user->mobile;
                 $user->save();
             } else {
-                $isCreate = true;
                 $user = User::create([
                     'open_id' => ['wechat' => ['unionid' => $unionid]],
                     'username' => $mobile,
@@ -68,7 +67,6 @@ class UserController extends Controller
                 $user->username = empty($user->username) ? $mobile : $user->username;
                 $user->save();
             } else {
-                $isCreate = true;
                 $user = User::create([
                     'open_id' => ['wechat' => ['unionid' => $unionid]],
                     'username' => $mobile,
@@ -80,13 +78,32 @@ class UserController extends Controller
 
         // 推荐关系
         if ($punionid) {
-            if ($isCreate) {
-                $spreadRepository->create($user->id, $punionid);
-            } else {
-                $parent = $spreadRepository->getUserParent($user->id);
-                if (!$parent || $parent->parent_id == 0) {
-                    $spreadRepository->create($user->id, $punionid);
+            // 当前用户关系
+            $userSpread = Spread::where('user_id', $user->id)->first();
+            // 当前用户上级关系
+            $parentUserSpread = Spread::where('user_id', $punionid)->first();
+            // 如果上级不存在
+            if (!$parentUserSpread) {
+                $parentUserSpread = $spreadRepository->create($punionid);
+            }
+
+            if ($userSpread) {
+                // 如果当前没有上下级关系
+                if ($userSpread->parent_id == 0) {
+                    $userSpread->parent_id = $punionid;
+                    $userSpread->grandfather_id = $parentUserSpread->parent_id;
+                    $userSpread->save();
+
+                    // 更新师傅的徒弟数
+                    $spreadRepository->updateSonCount($punionid);
+
+                    // 更新太师的徒孙数
+                    if ($parentUserSpread->parent_id > 0) {
+                        $spreadRepository->updateGrandsonCount($parentUserSpread->parent_id);
+                    }
                 }
+            } else {
+                $spreadRepository->create($user->id, $punionid);
             }
         }
 
